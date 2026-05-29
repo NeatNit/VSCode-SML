d57c0bd177db0927e1cc662f24377c441435021e
Nitai Sasson <github.com.nr568@sl.neatnit.net>
2026-05-29 23:06:21 +0300

    Update and fix fetch-git.sh
    Add a comment to the end of each line because \r\n EOL broke the script.
    Update to 2026 repository.
    Update HW2 and HW3 paths to Spr2026.
    Use >> redirection to avoid overwriting the first write with the second.

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -1,7 +1,7 @@
-git init hw_files
-cd hw_files
-git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2025.git
-git config core.sparseCheckout true 
-echo "Spr25/HW/Homework2/" > .git/info/sparse-checkout
-echo "Spr25/HW/Homework3/" > .git/info/sparse-checkout 
-git pull origin master
+git init hw_files #
+cd hw_files #
+git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2026.git #
+git config core.sparseCheckout true #
+echo "Spr26/HW/Homework 2/" >> .git/info/sparse-checkout #
+echo "Spr26/HW/Homework 3/" >> .git/info/sparse-checkout #
+git pull origin master #
